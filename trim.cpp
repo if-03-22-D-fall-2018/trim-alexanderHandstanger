d5a98f771eaf6dc88644b50f597e0f34db88659d
alexanderHandstanger <zombeyfan.ah@gmail.com>
2018-12-19 15:46:41 +0000

    Finished

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,29 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+ #include "trim.h"
+ #include <string.h>
+ #include "stdio.h"
+ void 	trim (const char *source, char *trimmed_string)
+ {
+
+   if (strcmp(source,"") == 0){
+     strcpy(trimmed_string,"");
+   }
+
+   else{
+     int min = 0;
+     int count = 0;
+     int max = strlen(source)-1;
+     while (source[min] == ' '){
+       min++;
+     }
+     while(max >= min && source[max] == ' ')   {
+       max--;
+     }
+     for (int i = min; i <= max; i++) {
+       trimmed_string[count] = source[i];
+       count++;
+     }
+   }
+ }
